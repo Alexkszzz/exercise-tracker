8c9b8aed7d4fb24a401d475ad29722024c3dd2fe
Alex Mai <maitrongtuanbmt@gmail.com>
2024-05-13 03:40:24 +0000

    final commit. All tests passed

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ app.get('/', (req, res) => {
 });
 
 const exerciseSchema = new mongoose.Schema({
-  username:String,
+  username: String,
   description: String,
   duration: Number,
-  date: String
+  date: Date
 })
 
 const userSchema = new mongoose.Schema({
@@ -55,7 +55,87 @@ app.get('/api/users', function(req,res){
   })
 })
 
+app.post('/api/users/:_id/exercises', async function(req,res){
+  const {description, duration, date} = req.body
+  try {
+    const user = await Users.findById(req.params._id)
+    if (!user){
+      res.json({error: "User not found"})
+      return
+    }
+    // Create a new Date object
+    const exerciseDate = date ? new Date(date) : new Date();
 
+    // Format the date using toLocaleDateString() with the defined options
+    const username = user.username
+    let newExercise = new Exercises({
+      username: username,
+      description: description,
+      duration: duration,
+      date: exerciseDate
+    })
+
+    newExercise.save()
+    res.json({
+      username: username,
+      description: newExercise.description,
+      duration: newExercise.duration,
+      date: exerciseDate.toDateString(),
+      _id: user._id
+    })
+  }
+  
+  catch(err){
+    console.log(err)
+  }})
+
+app.get("/api/users/:_id/logs", async function (req,res){
+  let count = 0;
+  const {from, to, limit} = req.query
+  try {
+    const user = await Users.findById(req.params._id)
+    if (!user){
+      res.json({error: "User not found"})
+      return
+    }
+
+    const query = {username: user.username}
+    
+    let dateFilter ={}
+    // Add conditions for 'from' and 'to' dates if provided
+    if (from) {
+      const fromDate = new Date(from);
+      dateFilter["$gte"] = fromDate
+    } 
+    if (to) {
+      const toDate = new Date(to);
+      dateFilter["$lte"] = toDate
+    }
+
+    let exercisesQuery = Exercises.find(query)
+    if (limit) {
+      exercisesQuery = exercisesQuery.limit(+limit);
+    }
+    const exercises = await exercisesQuery
+    let log = exercises.map(e => ({
+      description: e.description,
+      duration: e.duration,
+      date: e.date.toDateString(),
+    }))
+    res.json({
+      username: user.username,
+      count: log.length,
+      _id: user._id,
+      log: log
+    })
+
+  }
+  catch(err) {
+    console.log(err)
+    res.json("Server Error")
+  }
+
+})
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
